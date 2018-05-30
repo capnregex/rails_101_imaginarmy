129d24f3672d4394d9763aa0b20ab3800b3cdf57
Robert Ferney <rob@ferney.org>
2018-05-30 12:23:08 -0600

    ranks table generated

---
+++ db/migrate/20180530182014_create_ranks.rb
@@ -0,0 +1,10 @@
+class CreateRanks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ranks do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :ranks, :name
+  end
+end
